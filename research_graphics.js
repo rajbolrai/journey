77fe9b34cd81ef733ed0fbb41af5bfb886d321e0
rajbolrai <rajbolrai@hotmail.com>
2025-06-20 15:44:31 +0100

    adding steps for  iwebgl

--- research_graphics.js
+++ research_graphics.js
@@ -4,12 +4,12 @@ function main(){
     const canvas = document.querySelector("#gl-canvas");
     const gl = canvas.getContext("webgl");
 
-    if(gl = null){
+    if(gl === null){
         alert(
-            "Unable to initialise WebGl."
+            "Unable to initialise WebGL."
         );
         return;
     }
-    gl.clearColor(0.0, 0.0, 1.0);
+    gl.clearColor(1, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
